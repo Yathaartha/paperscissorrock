cb909c163ffeff50a8cc91af4d1d4583ece52ecc
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-14 09:03:25 +0545

    adding computer choices and finding outcome

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const ROCK= "ROCK";
 const PAPER= "PAPER";
 const SCISSORS= "SCISSORS";
 const DEFAULT_USER_CHOICE = "ROCK";
+const RESULT_DRAW = "DRAW";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
 
 let gameIsRunning = false;
 
@@ -16,6 +19,27 @@ function getPlayerChoice() {
   return selection;
 }
 
+function getComputerChoice() {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+}
+
+const getWinner = function(cChoice, pChoice) {
+  if (cChoice === pChoice){
+    return RESULT_DRAW;
+  } else if (cChoice === ROCK && pChoice === PAPER || cChoice === PAPER && pChoice === SCISSORS || cChoice === SCISSORS && pChoice === ROCK){
+    return RESULT_PLAYER_WINS;
+  } else {
+    return RESULT_COMPUTER_WINS;
+  }
+}
+
 startGameBtn.addEventListener('click', function(){//anonymous function
   if (gameIsRunning) {
     return;
@@ -23,5 +47,7 @@ startGameBtn.addEventListener('click', function(){//anonymous function
   gameIsRunning = true;
   console.log("Game is starting...");
   const playerSelection = getPlayerChoice();
-  console.log(playerSelection);
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerSelection);
+  console.log(winner);
 });
