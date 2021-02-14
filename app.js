c63aab8c668a97915b1efc07e785ccce49682a55
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-14 08:51:03 +0545

    added input from user

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function() {//anonymous function
-//   console.log("Game is Starting");
-// };
+const ROCK= "ROCK";
+const PAPER= "PAPER";
+const SCISSORS= "SCISSORS";
+const DEFAULT_USER_CHOICE = "ROCK";
 
-function start() {
-  
+let gameIsRunning = false;
+
+function getPlayerChoice() {
+  const selection = prompt(`${ROCK}, ${SCISSORS} or ${PAPER}`, '').toUpperCase();
+  if (selection !== ROCK && selection !== SCISSORS && selection !== PAPER) {
+    alert(`Invalid choice! We chose ${ROCK} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
 }
 
-// const person = {
-//   name: "Max",
-//   greet: function greet() {
-//     console.log("Hello There");
-//   }
-// };
-
-// person.greet();
-
-// console.dir(startGame);
-
-startGameBtn.addEventListener('click', function(){
-  console.log("Game is starting...")
+startGameBtn.addEventListener('click', function(){//anonymous function
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is starting...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
